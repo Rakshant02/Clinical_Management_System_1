df7d337a6885dfc09ff0d0936fdde8a4e0eeddcb
Bisen <2460700@cognizant.com>
2026-02-06 18:31:03 +0530

    modified navbar.ts

--- frontend/src/app/shared/navbar/navbar.component.ts
+++ frontend/src/app/shared/navbar/navbar.component.ts
@@ -1,13 +1,33 @@
-
 // src/app/shared/navbar/navbar.component.ts
-import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { NgIf } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import {
+  NavigationEnd,
+  Router,
+  RouterLink,
+  RouterLinkActive,
+} from '@angular/router';
+//import { NgIf } from "../../../../node_modules/@angular/common/common_module.d-NEF7UaHr";
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink, RouterLinkActive],
-  standalone:true,
+  imports: [RouterLink, RouterLinkActive, NgIf],
+  standalone: true,
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
-export class NavbarComponent {}
+export class NavbarComponent implements OnInit {
+  showNavbar: boolean = true;
+  constructor(private router: Router) {}
+  ngOnInit(): void {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        if (event.url === '/login' || event.url === '/register') {
+          this.showNavbar = false;
+        } else {
+          this.showNavbar = true;
+        }
+      }
+    });
+  }
+}
