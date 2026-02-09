0dd60be7d600c0e4de63209d8afb02af99e37ad3
Sarayu <2460707@cognizant.com>
2026-02-09 15:44:47 +0530

    loginn pagess

--- frontend/src/app/shared/navbar/navbar.component.ts
+++ frontend/src/app/shared/navbar/navbar.component.ts
@@ -1,37 +1,3 @@
-// // src/app/shared/navbar/navbar.component.ts
-// import { NgIf } from '@angular/common';
-// import { Component, OnInit } from '@angular/core';
-// import {
-//   NavigationEnd,
-//   Router,
-//   RouterLink,
-//   RouterLinkActive,
-// } from '@angular/router';
-// //import { NgIf } from "../../../../node_modules/@angular/common/common_module.d-NEF7UaHr";
-
-// @Component({
-//   selector: 'app-navbar',
-//   imports: [RouterLink, RouterLinkActive, NgIf],
-//   standalone: true,
-//   templateUrl: './navbar.component.html',
-//   styleUrls: ['./navbar.component.css'],
-// })
-// export class NavbarComponent implements OnInit {
-//   showNavbar: boolean = true;
-//   constructor(private router: Router) {}
-//   ngOnInit(): void {
-//     this.router.events.subscribe((event) => {
-//       if (event instanceof NavigationEnd) {
-//         if (event.url === '/login' || event.url === '/register') {
-//           this.showNavbar = false;
-//         } else {
-//           this.showNavbar = true;
-//         }
-//       }
-//     });
-//   }
-// }
-// src/app/shared/navbar/navbar.component.ts
 import { NgIf } from '@angular/common';
 import { Component, OnDestroy, OnInit, inject } from '@angular/core';
 import { ActivatedRoute, NavigationEnd, Router, RouterLink, RouterLinkActive } from '@angular/router';
